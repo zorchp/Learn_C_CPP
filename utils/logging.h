2b34219e785a8d5d4230e464429843f3a1a77962
zorchp <zorchp@gmail.com>
2026-08-13 14:31:33 +0800

    add utils / vptr print

--- utils/logging.h
+++ utils/logging.h
@@ -1,10 +1,12 @@
 #include <unistd.h>
-#include <bits/stdc++.h>
-
+#include <chrono>
+#include <iomanip>
 
 inline std::string GetCurrentTimeMs() {
     auto now = std::chrono::system_clock::now();
-    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                  now.time_since_epoch()) %
+              1000;
     auto timer = std::chrono::system_clock::to_time_t(now);
 
     std::tm bt;
@@ -14,8 +16,8 @@ inline std::string GetCurrentTimeMs() {
     localtime_r(&timer, &bt);
 #endif
     std::ostringstream oss;
-    oss << std::put_time(&bt, "%Y-%m-%d %H:%M:%S") << "." << std::setfill('0') << std::setw(3)
-        << ms.count();
+    oss << std::put_time(&bt, "%Y-%m-%d %H:%M:%S") << "." << std::setfill('0')
+        << std::setw(3) << ms.count();
     return oss.str();
 }
 
@@ -29,7 +31,8 @@ inline FILE* GetLogFile() {
             fprintf(stderr, "Failed to open log file: %s\n", log_path);
             return stderr; // 改为stderr，避免和stdout混淆
         }
-        fprintf(f, "\n%s ================== Program Started ===================\n",
+        fprintf(f,
+                "\n%s ================== Program Started ===================\n",
                 GetCurrentTimeMs().c_str());
         fflush(f);
         return f;
@@ -37,12 +40,13 @@ inline FILE* GetLogFile() {
     return file;
 }
 
-#define MY_LOG(level, fmt, ...)                                                                  \
-    do {                                                                                      \
-        FILE* _f = GetLogFile();                                                              \
-        fprintf(_f, "[%s][" #level "][%s:%d][%s] " fmt "\n", GetCurrentTimeMs().c_str(),      \
-                __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__);                      \
-        fflush(_f);                                                                           \
+#define MY_LOG(level, fmt, ...)                                 \
+    do {                                                        \
+        FILE* _f = GetLogFile();                                \
+        fprintf(_f, "[%s][" #level "][%s:%d][%s] " fmt "\n",    \
+                GetCurrentTimeMs().c_str(), __FILE__, __LINE__, \
+                __PRETTY_FUNCTION__, ##__VA_ARGS__);            \
+        fflush(_f);                                             \
     } while (0)
 
 #define DLOG(fmt, ...) MY_LOG(DEBUG, fmt, ##__VA_ARGS__)
